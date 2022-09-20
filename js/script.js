c7e37ee545b385701697870de73a6cee594522eb
Costeira <joaocurveira13@gmail.com>
2022-09-20 21:32:41 +0100

    js update things

--- js/script.js
+++ js/script.js
@@ -1,45 +1,68 @@
+//hamburguer Menu
 const btn = document.getElementById('menu-btn');
+//sidemenu panel
 const sidemenu = document.getElementById('sidemenu');
 
-// Abrir e Fechar o SideMenu
-btn.addEventListener('click', () => {
-    btn.classList.toggle('open')
-    //nav.classList.toggle('flex')
-    //nav.classList.toggle('hidden')
+
+
+//Open and close Sidemenu
+function toggleSideMenu(){
+    btn.classList.toggle('open')  
     sidemenu.classList.toggle('open');
-});
+}
+btn.addEventListener('click',toggleSideMenu);
 
 
-function updateList() {
-    //Obter todas as sections
+
+
+function setCarouselBallByIndex(index){
+    document.querySelectorAll(".carrousel-selected").forEach(c => c.classList.remove("carrousel-selected"));
+    document.querySelectorAll(".carrousel-circle")[index].classList.add("carrousel-selected");
+}
+
+
+function changeSelectedCarousel() {
     const sections = [...document.querySelectorAll('section')].sort((a, b) => {
         return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top);
     });
-    document.querySelectorAll(".carrousel-selected").forEach(c => c.classList.remove("carrousel-selected"));
-    document.querySelectorAll(".carrousel-circle")[[...document.querySelectorAll('section')].indexOf(sections[0])].classList.add("carrousel-selected");
+    const selectedIndex = [...document.querySelectorAll('section')].indexOf(sections[0]);
+
+    setCarouselBallByIndex(selectedIndex);  // Update selected Ball
+    updateSidemenuByIndex(selectedIndex);   // Update Selected Section on the menu
 
 }
 
-updateList();
+//Scroll Listening
+changeSelectedCarousel();
 window.addEventListener('wheel', () => {
-    updateList();
+    changeSelectedCarousel();
 }, false)
 
 
 
-document.querySelectorAll(".carrousel-circle").forEach( ball =>{
+document.querySelectorAll(".carrousel-circle").forEach( (ball, ballIdx) =>{
 
     ball.addEventListener('click',() =>{
         document.querySelectorAll(".carrousel-selected").forEach(c => c.classList.remove("carrousel-selected"));
         ball.classList.add("carrousel-selected");
+        updateSidemenuByIndex(ballIdx);
     });
-    
+
 });
 
 
 
+function updateSidemenuByIndex(index){
+    document.querySelectorAll(".sidemenu-selected").forEach(c => c.classList.remove("sidemenu-selected"));
+    document.querySelectorAll(".sidemenu-a")[index].classList.add("sidemenu-selected");
+}
 
 
-
-
-
+document.querySelectorAll('.sidemenu-a').forEach( (anchor, anchorIndex) => {
+    anchor.addEventListener('click',() =>{
+        document.querySelectorAll(".sidemenu-selected").forEach(c => c.classList.remove("sidemenu-selected"));
+        anchor.classList.add("sidemenu-selected");
+        toggleSideMenu();
+        setCarouselBallByIndex(anchorIndex);
+    });
+});
