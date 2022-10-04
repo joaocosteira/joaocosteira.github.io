4105b03267c0a0aff525cb7831cf5e78da8d968a
Costeira <joaocurveira13@gmail.com>
2022-10-04 21:07:36 +0100

    music player v1

---
+++ js/music_player.js
@@ -0,0 +1,26 @@
+const playerButton = document.querySelector('.audio-player');
+const audio = document.querySelector('audio');  
+const time = document.querySelector('.timer');  
+const timeline = document.querySelector('.timeline');  
+
+const toggleAudio = () => {
+    playerButton.classList.toggle('playing');
+    audio.paused ? audio.play() : audio.pause();
+}
+playerButton.addEventListener('click', toggleAudio);
+audio.onended = () => {
+    playerButton.classList.remove('playing');
+    timeline.style.width = '0%';
+};
+
+
+const calculateTime = (secs) => {
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${Math.floor(secs / 60)}:${returnedSeconds}`;
+}
+
+audio.addEventListener('timeupdate', () => {
+    time.innerHTML = calculateTime(audio.duration - audio.currentTime);
+    timeline.style.width = `${(100 * audio.currentTime) / audio.duration}%`
+});
